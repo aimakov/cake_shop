7256ba683b92e3652bcc214f3803eeeb0e015dcb
aimakov <nurbolat.aimakov@gmail.com>
2022-05-11 21:21:31 +0900

    Adjusted burger menu - didn't finish responsiveness

--- src/Components/CakeBuilder/RenderedCake.js
+++ src/Components/CakeBuilder/RenderedCake.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { Icing, Layer, Cream } from "./cakeComponents";
+import { Icing, Layer, Cream } from "./CakeComponents";
 import "./CakeBuilder.css";
 
 const RenderedCake = (props) => {
-    return (
-        <div className={`RenderedCake ${props.small ? "smallCake" : null}`}>
-            <Icing src={props.IcingSrc} small={props.small} />
-            <Layer src={props.LayerSrc} first={true} small={props.small} />
+  return (
+    <div className={`RenderedCake ${props.small ? "smallCake" : null}`}>
+      <Icing src={props.IcingSrc} small={props.small} />
+      <Layer src={props.LayerSrc} first={true} small={props.small} />
+      <Cream src={props.CreamSrc} small={props.small} />
+      <Layer src={props.LayerSrc} small={props.small} />
+      {React.Children.toArray(
+        [...Array(props.NumLayers - 2)].map((x, i) => (
+          <>
             <Cream src={props.CreamSrc} small={props.small} />
             <Layer src={props.LayerSrc} small={props.small} />
-            {React.Children.toArray(
-                [...Array(props.NumLayers - 2)].map((x, i) => (
-                    <>
-                        <Cream src={props.CreamSrc} small={props.small} />
-                        <Layer src={props.LayerSrc} small={props.small} />
-                    </>
-                ))
-            )}
-        </div>
-    );
+          </>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default RenderedCake;
