c426b9ba08629c0f62affb6a67d9628374605649
aimakov <nurbolat.aimakov@gmail.com>
2022-05-17 01:58:50 +0900

    Resolving conflicts

--- src/Components/CakeBuilder/RenderedCake.js
+++ src/Components/CakeBuilder/RenderedCake.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { Icing, Layer, Cream } from "./CakeComponents";
+import { Icing, Layer, Cream } from "./cakeComponents";
 import "./CakeBuilder.css";
 
 const RenderedCake = (props) => {
-  return (
-    <div className={`RenderedCake ${props.small ? "smallCake" : null}`}>
-      <Icing src={props.IcingSrc} small={props.small} />
-      <Layer src={props.LayerSrc} first={true} small={props.small} />
-      <Cream src={props.CreamSrc} small={props.small} />
-      <Layer src={props.LayerSrc} small={props.small} />
-      {React.Children.toArray(
-        [...Array(props.NumLayers - 2)].map((x, i) => (
-          <>
+    return (
+        <div className={`RenderedCake ${props.small ? "smallCake" : null}`}>
+            <Icing src={props.IcingSrc} small={props.small} />
+            <Layer src={props.LayerSrc} first={true} small={props.small} />
             <Cream src={props.CreamSrc} small={props.small} />
             <Layer src={props.LayerSrc} small={props.small} />
-          </>
-        ))
-      )}
-    </div>
-  );
+            {React.Children.toArray(
+                [...Array(props.NumLayers - 2)].map((x, i) => (
+                    <>
+                        <Cream src={props.CreamSrc} small={props.small} />
+                        <Layer src={props.LayerSrc} small={props.small} />
+                    </>
+                ))
+            )}
+        </div>
+    );
 };
 
 export default RenderedCake;
--- src/Components/CakeBuilder/index.js
+++ src/Components/CakeBuilder/index.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
-import {
-  FiPlusCircle,
-  FiMinusCircle,
-  FiChevronRight,
-  FiMinus,
-  FiPlus,
-} from "react-icons/fi";
+import { FiPlusCircle, FiMinusCircle, FiChevronRight, FiMinus, FiPlus } from "react-icons/fi";
 import "./CakeBuilder.css";
 
-import { Layer, Cream, Icing } from "./CakeComponents";
+import { Layer, Cream, Icing } from "./cakeComponents";
 import RadioGroup from "./RadioGroup";
 import { CartContext } from "../../App";
 
@@ -18,161 +12,146 @@ import NumLayersControl from "./NumLayersControl";
 import CakesQuantity from "./CakesQuantity";
 
 const CakeBuilder = (props) => {
-  const [LayersType, setLayersType] = useState("Vanilla");
-  const [CreamType, setCreamType] = useState("Vanilla");
-  const [IcingType, setIcingType] = useState("Vanilla");
-  const [NumLayers, setNumLayers] = useState(2);
+    const [LayersType, setLayersType] = useState("Vanilla");
+    const [CreamType, setCreamType] = useState("Vanilla");
+    const [IcingType, setIcingType] = useState("Vanilla");
+    const [NumLayers, setNumLayers] = useState(2);
 
-  const [LayerSrc, setLayerSrc] = useState(cakeElements.layer.vanilla);
-  const [CreamSrc, setCreamSrc] = useState(cakeElements.cream.vanilla);
-  const [IcingSrc, setIcingSrc] = useState(cakeElements.icing.vanilla);
+    const [LayerSrc, setLayerSrc] = useState(cakeElements.layer.vanilla);
+    const [CreamSrc, setCreamSrc] = useState(cakeElements.cream.vanilla);
+    const [IcingSrc, setIcingSrc] = useState(cakeElements.icing.vanilla);
 
-  const [left, setLeft] = useState("-100vw");
+    const [left, setLeft] = useState("-100vw");
 
-  const cartValue = useContext(CartContext);
-  const [cart, setCart] = cartValue;
+    const cartValue = useContext(CartContext);
+    const [cart, setCart] = cartValue;
 
-  const [customCakeQuantity, setCustomCakeQuantity] = useState(1);
+    const [customCakeQuantity, setCustomCakeQuantity] = useState(1);
 
-  useEffect(() => {
-    if (IcingType === "Vanilla") {
-      setIcingSrc(cakeElements.icing.vanilla);
-    } else if (IcingType === "Chocolate") {
-      setIcingSrc(cakeElements.icing.chocolate);
-    } else if (IcingType === "Lemon") {
-      setIcingSrc(cakeElements.icing.lemon);
-    } else if (IcingType === "Strawberry") {
-      setIcingSrc(cakeElements.icing.strawberry);
-    }
-  }, [IcingType]);
+    useEffect(() => {
+        if (IcingType === "Vanilla") {
+            setIcingSrc(cakeElements.icing.vanilla);
+        } else if (IcingType === "Chocolate") {
+            setIcingSrc(cakeElements.icing.chocolate);
+        } else if (IcingType === "Lemon") {
+            setIcingSrc(cakeElements.icing.lemon);
+        } else if (IcingType === "Strawberry") {
+            setIcingSrc(cakeElements.icing.strawberry);
+        }
+    }, [IcingType]);
 
-  useEffect(() => {
-    if (LayersType === "Vanilla") {
-      setLayerSrc(cakeElements.layer.vanilla);
-    } else if (LayersType === "Chocolate") {
-      setLayerSrc(cakeElements.layer.chocolate);
-    } else if (LayersType === "Lemon") {
-      setLayerSrc(cakeElements.layer.lemon);
-    } else if (LayersType === "Strawberry") {
-      setLayerSrc(cakeElements.layer.strawberry);
-    }
-  }, [LayersType]);
+    useEffect(() => {
+        if (LayersType === "Vanilla") {
+            setLayerSrc(cakeElements.layer.vanilla);
+        } else if (LayersType === "Chocolate") {
+            setLayerSrc(cakeElements.layer.chocolate);
+        } else if (LayersType === "Lemon") {
+            setLayerSrc(cakeElements.layer.lemon);
+        } else if (LayersType === "Strawberry") {
+            setLayerSrc(cakeElements.layer.strawberry);
+        }
+    }, [LayersType]);
 
-  useEffect(() => {
-    if (CreamType === "Vanilla") {
-      setCreamSrc(cakeElements.cream.vanilla);
-    } else if (CreamType === "Chocolate") {
-      setCreamSrc(cakeElements.cream.chocolate);
-    } else if (CreamType === "Lemon") {
-      setCreamSrc(cakeElements.cream.lemon);
-    } else if (CreamType === "Strawberry") {
-      setCreamSrc(cakeElements.cream.strawberry);
-    }
-  }, [CreamType]);
+    useEffect(() => {
+        if (CreamType === "Vanilla") {
+            setCreamSrc(cakeElements.cream.vanilla);
+        } else if (CreamType === "Chocolate") {
+            setCreamSrc(cakeElements.cream.chocolate);
+        } else if (CreamType === "Lemon") {
+            setCreamSrc(cakeElements.cream.lemon);
+        } else if (CreamType === "Strawberry") {
+            setCreamSrc(cakeElements.cream.strawberry);
+        }
+    }, [CreamType]);
 
-  let AdditionalLayers = [];
+    let AdditionalLayers = [];
 
-  useEffect(() => {
-    AdditionalLayers = [];
-    for (let i = 2; i < NumLayers; i++) {
-      AdditionalLayers.push(
-        <>
-          <Cream src={CreamSrc} key={`${i}_cream`} />
-          <Layer src={LayerSrc} key={`${i}_layer`} />
-        </>
-      );
-    }
-  }, [NumLayers, LayersType, CreamType]);
+    useEffect(() => {
+        AdditionalLayers = [];
+        for (let i = 2; i < NumLayers; i++) {
+            AdditionalLayers.push(
+                <>
+                    <Cream src={CreamSrc} key={`${i}_cream`} />
+                    <Layer src={LayerSrc} key={`${i}_layer`} />
+                </>
+            );
+        }
+    }, [NumLayers, LayersType, CreamType]);
 
-  useEffect(() => {
-    if (props.active === "create") {
-      setLeft("0");
-    } else if (props.active === "sides") {
-      setLeft("-100vw");
-    } else if (props.active === "select") {
-      setLeft("-200vw");
-    }
-  }, [props.active]);
+    useEffect(() => {
+        if (props.active === "create") {
+            setLeft("0");
+        } else if (props.active === "sides") {
+            setLeft("-100vw");
+        } else if (props.active === "select") {
+            setLeft("-200vw");
+        }
+    }, [props.active]);
 
-  const addToCart = () => {
-    const order = {
-      type: "created",
-      numberOfLayers: NumLayers,
-      icingType: IcingType,
-      creamType: CreamType,
-      layerType: LayersType,
-      uid: generateUID(),
-      amount: customCakeQuantity,
+    const addToCart = () => {
+        const order = {
+            type: "created",
+            numberOfLayers: NumLayers,
+            icingType: IcingType,
+            creamType: CreamType,
+            layerType: LayersType,
+            uid: generateUID(),
+            amount: customCakeQuantity,
+        };
+
+        setCart((prevstate) => [...prevstate, order]);
     };
 
-    setCart((prevstate) => [...prevstate, order]);
-  };
+    const generateUID = () => {
+        while (true) {
+            let uid = Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
 
-  const generateUID = () => {
-    while (true) {
-      let uid = Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
+            if (cart.filter((element) => element.id === uid).length === 0) return uid;
+        }
+    };
 
-      if (cart.filter((element) => element.id === uid).length === 0) return uid;
-    }
-  };
+    useEffect(() => {
+        console.log(cart);
+    }, [cart]);
 
-  useEffect(() => {
-    console.log(cart);
-  }, [cart]);
-
-  return (
-    <>
-      <div className="CakeBuilderContainer" style={{ left: left }}>
-        <div className="CakeInside">
-          <div className="WithoutOrder">
-            <RadioGroup
-              LayersType={LayersType}
-              setLayersType={setLayersType}
-              CreamType={CreamType}
-              setCreamType={setCreamType}
-              IcingType={IcingType}
-              setIcingType={setIcingType}
+    return (
+        <>
+            <div className="CakeBuilderContainer" style={{ left: left }}>
+                <div className="CakeInside">
+                    <div className="WithoutOrder">
+                        <RadioGroup
+                            LayersType={LayersType}
+                            setLayersType={setLayersType}
+                            CreamType={CreamType}
+                            setCreamType={setCreamType}
+                            IcingType={IcingType}
+                            setIcingType={setIcingType}
+                        />
+                        <NumLayersControl NumLayers={NumLayers} setNumLayers={setNumLayers} />
+                        <RenderedCake IcingSrc={IcingSrc} LayerSrc={LayerSrc} CreamSrc={CreamSrc} NumLayers={NumLayers} />
+                    </div>
+                    <CakesQuantity CakeQuantity={customCakeQuantity} setCakeQuantity={setCustomCakeQuantity} className="CustomCakesQuantity" />
+                    <div className="AddToCartContainer">
+                        <button className="waves-effect waves-light btn-medium lighten-1 black-text AddToCart" onClick={addToCart}>
+                            ADD TO CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <FiChevronRight
+                onClick={() => props.setActive("sides")}
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "1%",
+                    fontSize: "3rem",
+                    cursor: "pointer",
+                }}
             />
-            <NumLayersControl
-              NumLayers={NumLayers}
-              setNumLayers={setNumLayers}
-            />
-            <RenderedCake
-              IcingSrc={IcingSrc}
-              LayerSrc={LayerSrc}
-              CreamSrc={CreamSrc}
-              NumLayers={NumLayers}
-            />
-          </div>
-          <CakesQuantity
-            CakeQuantity={customCakeQuantity}
-            setCakeQuantity={setCustomCakeQuantity}
-            className="CustomCakesQuantity"
-          />
-          <div className="AddToCartContainer">
-            <button
-              className="waves-effect waves-light btn-medium lighten-1 black-text AddToCart"
-              onClick={addToCart}
-            >
-              ADD TO CART
-            </button>
-          </div>
-        </div>
-      </div>
-      <FiChevronRight
-        onClick={() => props.setActive("sides")}
-        style={{
-          position: "absolute",
-          top: "50%",
-          right: "1%",
-          fontSize: "3rem",
-          cursor: "pointer",
-        }}
-      />
-    </>
-  );
+        </>
+    );
 };
 
 export default CakeBuilder;
